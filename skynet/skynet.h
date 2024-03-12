11849bf8b78e32fd671da5b600e5f9b72b9e16f0
fsossai <federico.sossai@gmail.com>
2024-03-12 18:09:56 -0500

    Passing by value for the time being

--- skynet/skynet.h
+++ skynet/skynet.h
@@ -59,7 +59,7 @@ public:
   }
 
   __attribute__((always_inline))
-  void append(const T& value) {
+  void append(T value) {
     int k = container_.size() - 1;
     PRAGMA_LDTC_BEGIN(k, next_k<T>, this);
     container_[k].push_back(value);
