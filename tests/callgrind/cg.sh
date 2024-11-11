dee35bd169a6e3d78227969dccfbe38eb9d5809f
fsossai <federico.sossai@gmail.com>
2024-11-11 17:11:59 -0600

    Better callgrind

--- tests/callgrind/cg.sh
+++ tests/callgrind/cg.sh
@@ -4,7 +4,9 @@ cd /nfs-scratch/fsv1684/repo/dt/tests
 
 export KMP_AFFINITY=granularity=thread,balanced
 
+source /nfs-scratch/fsv1684/repo/dt/enable
 source /nfs-scratch/fsv1684/noelle-dt/enable
+source /nfs-scratch/fsv1684/gino-dt/enable
 
 make bfs_omp FLAGS="-march=native -O3 -fno-exceptions" OUTDIR=callgrind
 
@@ -13,13 +15,15 @@ export OMP_NUM_THREADS=1
 cd callgrind
 
 input=../inputs/kronecker_24.bin
-bin=./bfs_omp.out
+bin=./bfs_tc.out
+
+echo $bin $input
 
 valgrind \
   --tool=callgrind \
   --simulate-cache=yes \
   $bin $input \
-  > cg_s.txt \
+  > cg_${jobid}_s.txt \
   2>&1
 
 export OMP_NUM_THREADS=8
@@ -28,5 +32,5 @@ valgrind \
   --tool=callgrind \
   --simulate-cache=yes \
   $bin $input \
-  > cg_p.txt \
+  > cg_${jobid}_p.txt \
   2>&1
