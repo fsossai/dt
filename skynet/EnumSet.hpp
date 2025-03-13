4055d60bda933da97e487b5d7c97a3e4de2d3529
fsossai <federico.sossai@gmail.com>
2025-03-13 18:40:33 -0500

    Improved `EnumSet`

--- skynet/EnumSet.hpp
+++ skynet/EnumSet.hpp
@@ -3,17 +3,18 @@
 #include <cstdint>
 #include <cstring>
 #include <vector>
+#include <cassert>
 
 #include "arcana/noelle/core/Pragma.h"
 
 namespace skynet {
 
-template <class T, int M = 1, size_t Padding = 1, bool Resizable = false>
+template <class T, size_t Padding = 1, bool Resizable = false>
 class EnumSet;
 
-template <class T, int M, size_t Padding, bool Resizable>
-void clause_enumset_insert(int N, EnumSet<T, M, Padding, Resizable> *enumset) {
-  int K = (N + M - 1) / M;
+template <class T, size_t Padding, bool Resizable>
+void clause_enumset_insert(int N, EnumSet<T, Padding, Resizable> *enumset) {
+  int K = (N + enumset->M_ - 1) / enumset->M_;
   auto currentK = enumset->buf_.size();
   if (currentK == K) {
     return;
@@ -26,16 +27,16 @@ void clause_enumset_insert(int N, EnumSet<T, M, Padding, Resizable> *enumset) {
   }
 }
 
-template <class T = size_t, int M, size_t Padding, bool Resizable>
+template <class T = size_t, size_t Padding, bool Resizable>
 class EnumSet {
 public:
-  EnumSet(size_t size) {
+  EnumSet(size_t size) : M_(1) {
     buf_.emplace_back(((size * Padding) + 7) / 8);
     capacity_ = buf_[0].size() * 8;
   }
 
   void __insert(int t, size_t value) {
-    auto &row = buf_[t / M];
+    auto &row = buf_[t / M_];
     if constexpr (Resizable) {
       if ((value * Padding) >= capacity_) {
         row.resize((value * Padding) / 8 + 1);
@@ -43,30 +44,23 @@ public:
       }
     }
 
-    if constexpr (M == 1) {
-      row[(value * Padding) / 8] |= 1ULL << ((value * Padding) % 8ULL);
-    } else {
-      __atomic_fetch_or(&row[(value * Padding) / 8],
-                        1ULL << ((value * Padding) % 8),
-                        __ATOMIC_RELAXED);
-      // uint8_t old_value;
-      // uint8_t new_value;
-      // uint8_t *addr = &row[(value * Padding) / 8];
-      // do {
-      //   old_value = *addr;
-      // new_value = old_value | (1 << ((value * Padding) % 8));
-      // } while (!__sync_bool_compare_and_swap(addr, old_value, new_value));
-    }
+    __atomic_fetch_or(&row[(value * Padding) / 8],
+                      1ULL << ((value * Padding) % 8),
+                      __ATOMIC_RELAXED);
+  }
+
+  void setSharing(int M) {
+    assert(M >= 1);
+    M_ = M;
   }
 
   __attribute__((always_inline)) void insert(size_t value) {
     int t = 0;
-    auto p =
-        noelle_pragma_begin("ldtc",
-                            &t,
-                            0,
-                            clause_enumset_insert<T, M, Padding, Resizable>,
-                            this);
+    auto p = noelle_pragma_begin("ldtc",
+                                 &t,
+                                 0,
+                                 clause_enumset_insert<T, Padding, Resizable>,
+                                 this);
     __insert(t, value);
     noelle_pragma_end(p);
   }
@@ -102,6 +96,7 @@ public:
   // private:
   std::vector<std::vector<uint8_t>> buf_;
   size_t capacity_;
+  int M_;
 };
 
 } // namespace skynet
