f8d4b38f6067f1cbd76477ab983ec9e1aa7df7be
fsossai <federico.sossai@gmail.com>
2025-04-21 15:42:15 -0500

    update_if --> update_if_eq

--- skynet/Array.hpp
+++ skynet/Array.hpp
@@ -83,7 +83,7 @@ public:
     noelle_pragma_end(p);
   }
 
-  bool update_if(size_t idx, T old_value, T new_value) {
+  bool update_if_eq(size_t idx, T old_value, T new_value) {
     auto p = noelle_pragma_begin("ldtc");
     auto *addr = &container_[idx];
     if (*addr == old_value) {
