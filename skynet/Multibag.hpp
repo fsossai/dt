e968d82812efe1767b5eecda7b530ced1efbf8fe
fsossai <federico.sossai@gmail.com>
2025-04-24 22:56:06 -0500

    `Multimap` vs `Multibag`

---
+++ skynet/Multibag.hpp
@@ -0,0 +1,226 @@
+#pragma once
+
+#include <absl/container/btree_map.h>
+#include <atomic>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+#include "Interface.hpp"
+#include "IV.hpp"
+#include "Range.hpp"
+#include "BaseSequence.hpp"
+
+#include "arcana/noelle/core/Pragma.h"
+
+namespace skynet {
+
+template <typename Tk, typename Tv, bool Order>
+class Multibag;
+
+template <typename Tk, typename Tv, bool Order>
+void clause_multibag_insert(int N, Multibag<Tk, Tv, Order> *mmap) {
+  if (mmap->N_ == N) {
+    return;
+  }
+  mmap->N_ = N;
+  for (auto &[key, bucket] : mmap->container_) {
+    clause_sequence_append(N, &bucket);
+  }
+}
+
+template <typename Tk, typename Tv, bool Order>
+class Multibag {
+public:
+  using BucketT = BaseSequence<Tv, /*Order*/ false>;
+  using MapT = typename std::conditional_t<Order,
+                                           // absl::btree_map<Tk, BucketT>,
+                                           std::map<Tk, BucketT>,
+                                           std::unordered_map<Tk, BucketT>>;
+  using MapIteratorT = typename MapT::iterator;
+
+  friend void clause_multibag_insert<Tk, Tv>(int N,
+                                             Multibag<Tk, Tv, Order> *mmap);
+
+  class KeysIterator {
+  public:
+    KeysIterator(MapIteratorT it) : it_(move(it)) {}
+
+    Tk operator*() {
+      return it_->first;
+    }
+
+    KeysIterator &operator++() {
+      ++it_;
+      return *this;
+    }
+
+    bool operator!=(const KeysIterator &other) const {
+      return it_ != other.it_;
+    }
+
+    typename MapT::difference_type operator-(const KeysIterator &other) const {
+      return it_ - other.it_;
+    }
+
+    KeysIterator operator+(int64_t a) const {
+      return { it_ + a };
+    }
+
+  private:
+    MapIteratorT it_;
+  };
+
+  class KeysView {
+  public:
+    KeysView(Multibag<Tk, Tv, Order> &mmap) : mmap_(mmap) {}
+
+    KeysIterator begin() {
+      return { mmap_.container_.begin() };
+    }
+
+    KeysIterator end() {
+      return { mmap_.container_.end() };
+    }
+
+  private:
+    Multibag<Tk, Tv, Order> &mmap_;
+  };
+
+  // TODO
+  Multibag() = default;
+
+  ~Multibag() {}
+
+  void insert(Tk key, const Tv &value) {
+    int k = 0;
+    auto p = noelle_pragma_begin("ldtc",
+                                 &k,
+                                 0,
+                                 clause_multibag_insert<Tk, Tv, Order>,
+                                 this);
+    __insert(k, key, value);
+    noelle_pragma_end(p);
+  }
+
+  void __insert(int t, Tk key, const Tv &value) {
+    auto it = container_.find(key);
+    if (it != container_.end()) {
+      BucketT &bucket = it->second;
+      bucket.__insert(t, value);
+    } else {
+      BucketT bucket;
+      clause_sequence_append(N_, &bucket);
+      bucket.__insert(t, value);
+      {
+        // critical section
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto it_retry = container_.find(key);
+        if (it_retry != container_.end()) {
+          BucketT &bucket = it_retry->second;
+          bucket.__insert(t, value);
+        } else {
+          container_.insert({ key, std::move(bucket) });
+        }
+      }
+    }
+  }
+
+  INLINE size_t numKeys() const {
+    return container_.size();
+  }
+
+  KeysView keys() {
+    return { *this };
+  }
+
+  template <typename R = KeysView>
+  typename std::enable_if_t<Order, R> INLINE sortedKeys() {
+    return { *this };
+  }
+
+  template <typename R = Tk>
+  typename std::enable_if_t<Order, R> INLINE minKey() {
+    return container_.begin()->first;
+  }
+
+  template <typename R = Tk>
+  typename std::enable_if_t<Order, R> maxKey() {
+    auto it = container_.begin();
+    auto end = container_.end();
+    Tk current_max = it->first;
+    for (; it != end; ++it) {
+      current_max = std::max<Tk>(current_max, it->first);
+    }
+    return current_max;
+  }
+
+  void compact() {
+    std::vector<Tk> toErase;
+    toErase.reserve(numKeys());
+    for (auto &[key, bucket] : container_) {
+      if (bucket.size() == 0) {
+        toErase.push_back(key);
+      }
+    }
+    for (auto key : toErase) {
+      container_.erase(key);
+    }
+  }
+
+  INLINE void erase(Tk key) {
+    container_.erase(key);
+  }
+
+  void printKeys() {
+    std::cout << "{ ";
+    for (auto k : keys()) {
+      std::cout << k << " ";
+    }
+    std::cout << "}\n";
+  }
+
+  INLINE bool hasKey(Tk key) const {
+    return container_.contains(key);
+  }
+
+  INLINE BucketT &operator[](Tk key) {
+    return container_[key];
+  }
+
+  size_t count(Tk key) {
+    auto it = container_.find(key);
+    if (it != container_.end()) {
+      BucketT &bucket = it->second;
+      return bucket.size();
+    }
+    return 0;
+  }
+
+  INLINE bool empty() const {
+    return container_.empty();
+  }
+
+  void print() const {
+    for (auto &[key, value] : container_) {
+      std::cout << key << " ";
+      value.printInternals();
+    }
+  }
+
+  // private:
+  MapT container_;
+  std::mutex mutex_;
+  int N_ = 1;
+};
+
+template <typename Tk, typename Tv>
+using OrderedMultibag = Multibag<Tk, Tv, /*Order=*/true>;
+
+template <typename Tk, typename Tv>
+using UnorderedMultibag = Multibag<Tk, Tv, /*Order=*/false>;
+
+} // namespace skynet
--- skynet/Multimap.hpp
+++ skynet/Multimap.hpp
@@ -18,80 +18,27 @@
 
 namespace skynet {
 
-template <typename Tk, typename Tv, bool Order>
+template <typename Tk, typename Tv>
 class Multimap;
 
-template <typename Tk, typename Tv, bool Order>
-void clause_multimap_insert(int N, Multimap<Tk, Tv, Order> *mmap) {
-  if (mmap->N_ == N) {
+template <typename Tk, typename Tv>
+void clause_multimap_insert(int N, Multimap<Tk, Tv> *mmap) {
+  if (mmap->container_.size() == N) {
     return;
   }
-  mmap->N_ = N;
-  for (auto &[key, bucket] : mmap->container_) {
-    clause_sequence_append(N, &bucket);
-  }
+  mmap->container_.resize(N);
 }
 
-template <typename Tk, typename Tv, bool Order>
+template <typename Tk, typename Tv>
 class Multimap {
 public:
-  using BucketT = BaseSequence<Tv, /*Order*/ false>;
-  using MapT = typename std::conditional_t<Order,
-                                           // absl::btree_map<Tk, BucketT>,
-                                           std::map<Tk, BucketT>,
-                                           std::unordered_map<Tk, BucketT>>;
+  using BucketT = std::vector<Tv>;
+  using MapT = std::unordered_map<Tk, BucketT>;
   using MapIteratorT = typename MapT::iterator;
 
-  friend void clause_multimap_insert<Tk, Tv>(int N,
-                                             Multimap<Tk, Tv, Order> *mmap);
+  friend void clause_multimap_insert<Tk, Tv>(int N, Multimap<Tk, Tv> *mmap);
 
-  class KeysIterator {
-  public:
-    KeysIterator(MapIteratorT it) : it_(move(it)) {}
-
-    Tk operator*() {
-      return it_->first;
-    }
-
-    KeysIterator &operator++() {
-      ++it_;
-      return *this;
-    }
-
-    bool operator!=(const KeysIterator &other) const {
-      return it_ != other.it_;
-    }
-
-    typename MapT::difference_type operator-(const KeysIterator &other) const {
-      return it_ - other.it_;
-    }
-
-    KeysIterator operator+(int64_t a) const {
-      return { it_ + a };
-    }
-
-  private:
-    MapIteratorT it_;
-  };
-
-  class KeysView {
-  public:
-    KeysView(Multimap<Tk, Tv, Order> &mmap) : mmap_(mmap) {}
-
-    KeysIterator begin() {
-      return { mmap_.container_.begin() };
-    }
-
-    KeysIterator end() {
-      return { mmap_.container_.end() };
-    }
-
-  private:
-    Multimap<Tk, Tv, Order> &mmap_;
-  };
-
-  // TODO
-  Multimap() = default;
+  Multimap() : container_(1) {}
 
   ~Multimap() {}
 
@@ -100,127 +47,116 @@ public:
     auto p = noelle_pragma_begin("ldtc",
                                  &k,
                                  0,
-                                 clause_multimap_insert<Tk, Tv, Order>,
+                                 clause_multimap_insert<Tk, Tv>,
                                  this);
     __insert(k, key, value);
     noelle_pragma_end(p);
   }
 
   void __insert(int t, Tk key, const Tv &value) {
-    auto it = container_.find(key);
-    if (it != container_.end()) {
-      BucketT &bucket = it->second;
-      bucket.__insert(t, value);
+    auto it = container_[t].find(key);
+    if (it != container_[t].end()) {
+      it->second.push_back(value);
     } else {
-      BucketT bucket;
-      clause_sequence_append(N_, &bucket);
-      bucket.__insert(t, value);
-      {
-        // critical section
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto it_retry = container_.find(key);
-        if (it_retry != container_.end()) {
-          BucketT &bucket = it_retry->second;
-          bucket.__insert(t, value);
-        } else {
-          container_.insert({ key, std::move(bucket) });
-        }
-      }
+      container_[t].emplace(key, BucketT({ value }));
     }
   }
 
-  INLINE size_t numKeys() const {
-    return container_.size();
+  Tk minKey() {
+    Tk current_min = std::numeric_limits<Tk>::min();
+#pragma omp parallel for reduction(min : current_min)
+    for (auto &block : container_) {
+      for (auto &[key, _] : block) {
+        if (key < current_min) {
+          current_min = key;
+        }
+      }
+    }
+    return current_min;
   }
 
-  KeysView keys() {
-    return { *this };
-  }
-
-  template <typename R = KeysView>
-  typename std::enable_if_t<Order, R> INLINE sortedKeys() {
-    return { *this };
-  }
-
-  template <typename R = Tk>
-  typename std::enable_if_t<Order, R> INLINE minKey() {
-    return container_.begin()->first;
-  }
-
-  template <typename R = Tk>
-  typename std::enable_if_t<Order, R> maxKey() {
-    auto it = container_.begin();
-    auto end = container_.end();
-    Tk current_max = it->first;
-    for (; it != end; ++it) {
-      current_max = std::max<Tk>(current_max, it->first);
+  Tk maxKey() {
+    Tk current_max = std::numeric_limits<Tk>::max();
+#pragma omp parallel for reduction(max : current_max)
+    for (auto &block : container_) {
+      for (auto &[key, _] : block) {
+        if (key > current_max) {
+          current_max = key;
+        }
+      }
     }
     return current_max;
   }
 
-  void compact() {
-    std::vector<Tk> toErase;
-    toErase.reserve(numKeys());
-    for (auto &[key, bucket] : container_) {
-      if (bucket.size() == 0) {
-        toErase.push_back(key);
+  void erase(Tk key) {
+#pragma omp parallel for
+    for (auto &block : container_) {
+      block.erase(key);
+    }
+  }
+
+  void printInternals() {
+    std::cout << "{\n";
+    for (auto &block : container_) {
+      std::cout << "  { ";
+      for (auto &[key, bucket] : block) {
+        std::cout << "(" << key << ", { ";
+        for (auto &x : bucket) {
+          std::cout << x << " ";
+        }
+        std::cout << "})";
       }
-    }
-    for (auto key : toErase) {
-      container_.erase(key);
-    }
-  }
-
-  INLINE void erase(Tk key) {
-    container_.erase(key);
-  }
-
-  void printKeys() {
-    std::cout << "{ ";
-    for (auto k : keys()) {
-      std::cout << k << " ";
+      std::cout << " }\n";
     }
     std::cout << "}\n";
   }
 
-  INLINE bool hasKey(Tk key) const {
-    return container_.contains(key);
-  }
+  BucketT operator[](Tk key) {
+    BucketT full_bucket(count(key));
+    std::atomic<size_t> offset = 0;
+#pragma omp parallel for
+    for (auto &block : container_) {
+      auto it = block.find(key);
+      if (it != block.end()) {
+        auto &current_bucket = it->second;
+        auto start =
+            offset.fetch_add(current_bucket.size(), std::memory_order_relaxed);
 
-  INLINE BucketT &operator[](Tk key) {
-    return container_[key];
+        std::copy(current_bucket.begin(),
+                  current_bucket.end(),
+                  full_bucket.data() + start);
+      }
+    }
+    return full_bucket;
   }
 
   size_t count(Tk key) {
-    auto it = container_.find(key);
-    if (it != container_.end()) {
-      BucketT &bucket = it->second;
-      return bucket.size();
+    size_t counter = 0;
+#pragma omp parallel for reduction(+ : counter)
+    for (auto &block : container_) {
+      auto it = block.find(key);
+      if (it != block.end()) {
+        counter += it->second.size();
+      }
     }
-    return 0;
+    return counter;
   }
 
-  INLINE bool empty() const {
-    return container_.empty();
-  }
-
-  void print() const {
-    for (auto &[key, value] : container_) {
-      std::cout << key << " ";
-      value.printInternals();
+  bool empty() {
+    for (auto &block : container_) {
+      if (block.size() != 0) {
+        for (auto &[_, bucket] : block) {
+          if (bucket.size() != 0) {
+            return false;
+          }
+        }
+      }
     }
+    return true;
   }
 
   // private:
-  MapT container_;
-  std::mutex mutex_;
-  int N_ = 1;
+  std::vector<MapT> container_;
 };
-
-template <typename Tk, typename Tv>
-using OrderedMultimap = Multimap<Tk, Tv, /*Order=*/true>;
-
-template <typename Tk, typename Tv>
-using UnorderedMultimap = Multimap<Tk, Tv, /*Order=*/false>;
 
 } // namespace skynet
