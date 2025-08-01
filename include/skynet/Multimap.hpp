99c42c146d7f088ac8e934c4e0b86c64672037d2
fsossai <federico.sossai@gmail.com>
2025-08-01 16:54:00 -0500

    Added padding for Multimap

--- include/skynet/Multimap.hpp
+++ include/skynet/Multimap.hpp
@@ -18,27 +18,32 @@
 
 namespace skynet {
 
-template <typename Tk, typename Tv>
+template <typename T>
+using Bucket = std::vector<T>;
+
+template <typename Tk, typename Tv, uint32_t PAD = compute_padding<Bucket<Tv>>()>
 class Multimap;
 
-template <typename Tk, typename Tv>
-void clause_multimap_insert(int N, Multimap<Tk, Tv> *mmap) {
+template <typename Tk, typename Tv, uint32_t PAD>
+void clause_multimap_insert(int N, Multimap<Tk, Tv, PAD> *mmap) {
+  N *= PAD;
   if (mmap->container_.size() == N) {
     return;
   }
   mmap->container_.resize(N);
 }
 
-template <typename Tk, typename Tv>
+template <typename Tk, typename Tv, uint32_t PAD>
 class Multimap {
 public:
-  using BucketT = std::vector<Tv>;
+  using BucketT = Bucket<Tv>;
   using MapT = std::unordered_map<Tk, BucketT>;
   using MapIteratorT = typename MapT::iterator;
 
-  friend void clause_multimap_insert<Tk, Tv>(int N, Multimap<Tk, Tv> *mmap);
+  friend void clause_multimap_insert<Tk, Tv, PAD>(int N,
+                                                  Multimap<Tk, Tv, PAD> *mmap);
 
-  Multimap() : container_(1) {}
+  Multimap() : container_(PAD) {}
 
   ~Multimap() {}
 
@@ -47,25 +52,26 @@ public:
     auto p = noelle_pragma_begin("ldtc",
                                  &k,
                                  0,
-                                 clause_multimap_insert<Tk, Tv>,
+                                 clause_multimap_insert<Tk, Tv, PAD>,
                                  this);
     __insert(k, key, value);
     noelle_pragma_end(p);
   }
 
   void __insert(int t, Tk key, Tv value) {
-    auto it = container_[t].find(key);
-    if (it != container_[t].end()) {
+    auto it = container_[t * PAD].find(key);
+    if (it != container_[t * PAD].end()) {
       it->second.push_back(value);
     } else {
-      container_[t].emplace(key, BucketT({ value }));
+      container_[t * PAD].emplace(key, BucketT({ value }));
     }
   }
 
   Tk minKey() {
     Tk current_min = std::numeric_limits<Tk>::min();
 #pragma omp parallel for reduction(min : current_min)
-    for (auto &block : container_) {
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      auto &block = container_[i * PAD];
       for (auto &[key, _] : block) {
         if (key < current_min) {
           current_min = key;
@@ -78,7 +84,8 @@ public:
   Tk maxKey() {
     Tk current_max = std::numeric_limits<Tk>::max();
 #pragma omp parallel for reduction(max : current_max)
-    for (auto &block : container_) {
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      auto &block = container_[i * PAD];
       for (auto &[key, _] : block) {
         if (key > current_max) {
           current_max = key;
@@ -90,14 +97,15 @@ public:
 
   void erase(Tk key) {
 #pragma omp parallel for
-    for (auto &block : container_) {
-      block.erase(key);
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      container_[i * PAD].erase(key);
     }
   }
 
   void printInternals() {
     std::cout << "{\n";
-    for (auto &block : container_) {
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      auto &block = container_[i * PAD];
       std::cout << "  { ";
       for (auto &[key, bucket] : block) {
         std::cout << "(" << key << ", { ";
@@ -113,9 +121,10 @@ public:
 
   BucketT operator[](Tk key) {
     BucketT full_bucket(count(key));
-    std::atomic<size_t> offset = 0;
+    std::atomic<size_t> offset(0);
 #pragma omp parallel for
-    for (auto &block : container_) {
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      auto &block = container_[i * PAD];
       auto it = block.find(key);
       if (it != block.end()) {
         auto &current_bucket = it->second;
@@ -133,7 +142,8 @@ public:
   size_t count(Tk key) {
     size_t counter = 0;
 #pragma omp parallel for reduction(+ : counter)
-    for (auto &block : container_) {
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      auto &block = container_[i * PAD];
       auto it = block.find(key);
       if (it != block.end()) {
         counter += it->second.size();
@@ -142,24 +152,9 @@ public:
     return counter;
   }
 
-  //   bool empty() {
-  //     bool result = true;
-  // #pragma omp parallel for reduction(&& : result)
-  //     for (auto &block : container_) {
-  //       if (block.size() != 0) {
-  //         for (auto &[_, bucket] : block) {
-  //           if (bucket.size() != 0) {
-  //             result &= false;
-  //             break;
-  //           }
-  //         }
-  //       }
-  //     }
-  //     return result;
-  //   }
-
   bool empty() {
-    for (auto &block : container_) {
+    for (int i = 0; i < container_.size() / PAD; i++) {
+      auto &block = container_[i * PAD];
       if (block.size() != 0) {
         for (auto &[_, bucket] : block) {
           if (bucket.size() != 0) {
