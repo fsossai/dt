4243659d7170a337cf1739ca56b9036019299360
fsossai <federico.sossai@gmail.com>
2025-08-01 16:10:41 -0500

    Smarter padding

--- include/skynet/Common.hpp
+++ include/skynet/Common.hpp
@@ -7,8 +7,15 @@
 
 const uint32_t L1D_CACHE_LINE_SIZE = 64;
 
+constexpr uint32_t gcd(uint32_t a, uint32_t b) {
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+constexpr uint32_t lcm(uint32_t a, uint32_t b) {
+  return (a / gcd(a, b)) * b;
+}
+
 template <typename T>
 constexpr uint32_t compute_padding() {
-  return std::max<uint32_t>((L1D_CACHE_LINE_SIZE + sizeof(T) - 1) / sizeof(T),
-                            1U);
+  return lcm(L1D_CACHE_LINE_SIZE, sizeof(T)) / sizeof(T);
 }
