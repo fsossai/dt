a6dfd7c1f820a535ede2b1651c6ebca8693fb7ce
fsossai <federico.sossai@gmail.com>
2025-08-04 19:12:16 -0500

    Added tail padding in MArray

--- include/skynet/MArray.hpp
+++ include/skynet/MArray.hpp
@@ -32,10 +32,14 @@ void clause_array_add(int N, MArray<T> *array) {
 #endif
 
   assert(currentK < K);
+  auto ceil_div = [](size_t a, size_t b) { return (a + b - 1) / b; };
+
   array->container_.resize(K);
   auto L = array->size_;
   for (size_t i = currentK; i < K; i++) {
     auto &new_row = array->container_[i];
+    const auto padding_elements = ceil_div(MArray<T>::TAIL_PADDING, sizeof(T));
+    new_row.reserve(L + MArray<T>::TAIL_PADDING);
     new_row.resize(L, array->default_value_);
   }
 }
@@ -75,6 +79,8 @@ public:
                                         MArray<T> *base,
                                         int M,
                                         int offset);
+
+  static constexpr size_t TAIL_PADDING = 4000; // bytes
 
   class Iterator {
   public:
