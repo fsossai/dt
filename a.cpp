9cb27116b0216ed4d6c34a87d92c255fc51b1d27
fsossai <federico.sossai@gmail.com>
2025-08-05 00:48:39 -0500

    Fix

--- a.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <map>
-
-int main() {
-    std::cout << "sizeof(unordered_map<int,int>): " 
-              << sizeof(std::map<int, int>) << std::endl;
-    return 0;
-}
