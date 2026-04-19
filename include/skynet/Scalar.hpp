548756b72116d5c1c9e8c809d7a63838edc1a625
fsossai <federico.sossai@gmail.com>
2026-04-19 01:12:55 -0500

    scalar: added max

--- include/skynet/Scalar.hpp
+++ include/skynet/Scalar.hpp
@@ -32,7 +32,23 @@ void clause_scalar_min(int N, Scalar<T, PAD> *s) {
 }
 
 template <class T, uint32_t PAD>
+void clause_scalar_max(int N, Scalar<T, PAD> *s) {
+  if (s->container_.size() / PAD == N) {
+    return;
+  }
+  s->container_.resize(N * PAD);
+}
+
+template <class T, uint32_t PAD>
 void clause_scalar_keepMin(int N, Scalar<T, PAD> *s) {
+  auto value = s->get();
+  s->container_.resize(1);
+  auto &lane0 = s->container_[0];
+  lane0 = value;
+}
+
+template <class T, uint32_t PAD>
+void clause_scalar_keepMax(int N, Scalar<T, PAD> *s) {
   auto value = s->get();
   s->container_.resize(1);
   auto &lane0 = s->container_[0];
@@ -43,7 +59,10 @@ template <class T, uint32_t PAD>
 class Scalar {
 public:
   friend void clause_scalar_add<T, PAD>(int N, Scalar<T, PAD> *s);
+  friend void clause_scalar_min<T, PAD>(int N, Scalar<T, PAD> *s);
+  friend void clause_scalar_max<T, PAD>(int N, Scalar<T, PAD> *s);
   friend void clause_scalar_keepMin<T, PAD>(int N, Scalar<T, PAD> *s);
+  friend void clause_scalar_keepMax<T, PAD>(int N, Scalar<T, PAD> *s);
 
   Scalar(T x) : container_{ x } {}
 
@@ -73,6 +92,18 @@ public:
     noelle_pragma_end(_p);
   }
 
+  INLINE void max(T x) {
+    size_t k = 0;
+    auto _p = noelle_pragma_begin("ldtc",
+                                  &k,
+                                  (size_t)0,
+                                  clause_scalar_max<T, PAD>,
+                                  this);
+    auto &lane = container_[k * PAD];
+    lane = (lane < x) ? x : lane;
+    noelle_pragma_end(_p);
+  }
+
   void set(T x) {
     std::memset(container_.data(), 0x0, container_.size() * sizeof(T));
     auto &lane0 = container_[0];
@@ -89,6 +120,25 @@ public:
     auto *lane_addr = &container_[0];
     auto current_lane = *lane_addr;
     while (current_lane > new_value) {
+      if (__sync_bool_compare_and_swap(lane_addr, current_lane, new_value)) {
+        return true;
+      }
+      current_lane = *lane_addr;
+    }
+    noelle_pragma_end(p);
+    return false;
+  }
+
+  bool keepMax(T new_value) {
+    int k;
+    auto p = noelle_pragma_begin("ldtc",
+                                 &k,
+                                 (int)0,
+                                 clause_scalar_keepMax<T, PAD>,
+                                 this);
+    auto *lane_addr = &container_[0];
+    auto current_lane = *lane_addr;
+    while (current_lane < new_value) {
       if (__sync_bool_compare_and_swap(lane_addr, current_lane, new_value)) {
         return true;
       }
@@ -158,6 +208,11 @@ public:
     lane = (lane < x) ? lane : x;
   }
 
+  INLINE void __max(size_t k, T x) {
+    auto &lane = container_[k * PAD];
+    lane = (lane < x) ? x : lane;
+  }
+
   void reduce() {
     auto &lane0 = container_[0];
     lane0 = get();
@@ -170,6 +225,16 @@ public:
     for (size_t i = 1; i < P; i++) {
       const auto lane = container_[i * PAD];
       lane0 = (lane0 < lane) ? lane0 : lane;
+    }
+    container_.resize(1 * PAD);
+  }
+
+  void reduce_max() {
+    auto &lane0 = container_[0];
+    const size_t P = container_.size() / PAD;
+    for (size_t i = 1; i < P; i++) {
+      const auto lane = container_[i * PAD];
+      lane0 = (lane0 < lane) ? lane : lane0;
     }
     container_.resize(1 * PAD);
   }
