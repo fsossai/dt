baf7a788fbe5e674bafc4c0e06c7dbda88487e0d
fsossai <federico.sossai@gmail.com>
2026-05-02 16:10:23 -0500

    backpack: explicit ctor

--- include/skynet/Backpack.hpp
+++ include/skynet/Backpack.hpp
@@ -17,7 +17,7 @@ namespace skynet {
 template <typename T>
 class Backpack {
 public:
-  Backpack(size_t N) : container_(new T[N]), N_(N), size_(0) {}
+  explicit Backpack(size_t N) : container_(new T[N]), N_(N), size_(0) {}
 
   Backpack(const Backpack &other)
     : container_(new T[other.N_]),
